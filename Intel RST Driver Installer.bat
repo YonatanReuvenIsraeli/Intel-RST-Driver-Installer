28dad12a929cd0380645cf0dbf66e9fab13df778
YonatanReuvenIsraeli <156263968+YonatanReuvenIsraeli@users.noreply.github.com>
2024-01-17 13:08:37 -0500

    Update Intel RST Driver Installer.bat

--- Intel RST Driver Installer.bat
+++ Intel RST Driver Installer.bat
@@ -17,25 +17,53 @@ goto :Start
 echo.
 echo Please download the following driver from here then press any key to continue. ^-^-^> https://www.intel.com/content/www/us/en/download/720755/intel-rapid-storage-technology-driver-installation-software-with-intel-optane-memory-11th-up-to-13th-gen-platforms.html
 pause
-goto :Done
+goto :PathWindows
 
 :Driver2
 echo.
 echo Please download the following driver from here then press any key to continue. ^-^-^> https://www.intel.com/content/www/us/en/download/19512/intel-rapid-storage-technology-driver-installation-software-with-intel-optane-memory-10th-and-11th-gen-platforms.html?v=t
 pause
-goto :Done
+goto :PathWindows
+
+:PathWindows
+echo.
+set /p Path="What is the path to your downloaded Intel RST driver? "
+set /p Windows="What is the drive letter of your Windows installation media? (A:-Z:) "
+if /i "%Windows%"=="A:" goto :Done
+if /i "%Windows%"=="B:" goto :Done
+if /i "%Windows%"=="C:" goto :Done
+if /i "%Windows%"=="D:" goto :Done
+if /i "%Windows%"=="E:" goto :Done
+if /i "%Windows%"=="F:" goto :Done
+if /i "%Windows%"=="G:" goto :Done
+if /i "%Windows%"=="H:" goto :Done
+if /i "%Windows%"=="I:" goto :Done
+if /i "%Windows%"=="J:" goto :Done
+if /i "%Windows%"=="K:" goto :Done
+if /i "%Windows%"=="L:" goto :Done
+if /i "%Windows%"=="M:" goto :Done
+if /i "%Windows%"=="N:" goto :Done
+if /i "%Windows%"=="O:" goto :Done
+if /i "%Windows%"=="P:" goto :Done
+if /i "%Windows%"=="Q:" goto :Done
+if /i "%Windows%"=="R:" goto :Done
+if /i "%Windows%"=="S:" goto :Done
+if /i "%Windows%"=="T:" goto :Done
+if /i "%Windows%"=="U:" goto :Done
+if /i "%Windows%"=="V:" goto :Done
+if /i "%Windows%"=="W:" goto :Done
+if /i "%Windows%"=="X:" goto :Done
+if /i "%Windows%"=="Y:" goto :Done
+if /i "%Windows%"=="Z:" goto :Done
+echo Invalid Syntax!
+goto :PathWindows
 
 :Done
+%Path% -extractdrivers %Windows%\SetupRST_extracted
+if errorlevel 1 goto :PathWindows
+move %Path% %Windows%
 echo.
-set /p Path="What is the path to your downloaded Intel RST driver (don't include the file name. Only include the path to the folder that the Intel RST driver is in)? "
-set /p Windows="What is the drive letter of your Windows installation media? "
-cd %Path%
-SetupRST.exe -extractdrivers %Windows%\SetupRST_extracted
-if errorlevel 1 goto :Done
-move SetupRST.exe %Windows%
-if errorlevel 1 goto :Done
-echo.
-echo Your Windows installation media now has the Intel RST driver. You can now load the Intel RST driver (it's in %Windows%\SetupRST_extracted) from within the Windows setup. Once the system boots into the OS, run the SetupRST.exe file from the root directory of your Windows installation media. This will install the Windows driver and give the opportunity to download the Intel Optane Memory and Storage Management application from the Microsoft Store for management of RAID/Intel Optane memory volumes. Press any key to close this batch file.
+echo Your Windows installation media now has the Intel RST driver. You can now load the Intel RST driver (it's in %Windows%\SetupRST_extracted) from within the Windows setup. Once the system boots into the OS, run the SetupRST.exe file (it's in %Windows%\SetupRST.exe). This will install the Windows driver and give the opportunity to download the Intel Optane Memory and Storage Management application from the Microsoft Store for management of RAID/Intel Optane memory volumes. Press any key to close this batch file.
 endlocal
 pause
 exit

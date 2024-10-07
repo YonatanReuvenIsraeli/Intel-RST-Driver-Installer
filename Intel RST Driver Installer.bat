e92f0c430bea414846748d9b0b4a6c1b7cccdc56
Yonatan Israeli <yonirisraeli@gmail.com>
2024-10-06 22:28:42 -0400

    Update Intel RST Driver Installer.bat

--- Intel RST Driver Installer.bat
+++ Intel RST Driver Installer.bat
@@ -2,7 +2,7 @@
 title Intel RST Driver Installer
 setlocal
 echo Program Name: Intel RST Driver Installer
-echo Version: 1.4.8
+echo Version: 1.4.9
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -112,9 +112,9 @@ echo Installing Intel RST driver.
 if not errorlevel 0 goto Error
 move "%RSTPath%\SetupRST.exe" "%Windows%" > nul 2>&1
 if not errorlevel 0 goto Error
-endlocal
 echo.
-echo Your Windows installation media now has the Intel RST driver. You can now load the Intel RST driver (it's in "%Windows%\SetupRST_extracted") from within the Windows setup. Once the system boots into the OS, run the SetupRST.exe file (it's in %Windows%\SetupRST.exe). This will install the Windows driver and give the opportunity to download the Intel Optane Memory and Storage Management application from the Microsoft Store for management of RAID/Intel Optane memory volumes. Press any key to close this batch file.
+echo Your Windows installation media now has the Intel RST driver. You can now load the Intel RST driver (it's in "%Windows%\SetupRST_extracted") from within the Windows setup. Once the system boots into the OS, run the SetupRST.exe file (it's in "%Windows%\SetupRST.exe"). This will install the Windows driver and give the opportunity to download the Intel Optane Memory and Storage Management application from the Microsoft Store for management of RAID/Intel Optane memory volumes. Press any key to close this batch file.
+endlocal
 pause > nul 2>&1
 exit
 
